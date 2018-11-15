d42e754e1269ed3439633843e850af568f2d516c
pavel <pavels.vasiluks@gmail.com>
2018-11-15 10:57:42 +0200

    Added DI Configuration

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace InfiniteSoftware\Bundle\SuluArticlesPublisherBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('sulu_articles_publisher');
+        return $treeBuilder;
+    }
+}
